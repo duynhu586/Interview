9bbcf5b3314aaf9999dcc7425c3bdff47d2bf93c
Nguyen Duy <duynhu586@gmail.com>
2026-04-18 16:49:44 +0700

    init: create product list screen

---
+++ quanlygom/lib/views/product_list/product_list_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProductListScreen extends StatelessWidget {
+  const ProductListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Product List Screen',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
